7506ef799101770edca908f2f289928d0b8a521f
HectorPalacio <palaciorivas@gmail.com>
2020-08-29 15:44:56 -0500

    Implementar CRUD hacia servicios REST

---
+++ lib/src/pages/producto_page.dart
@@ -0,0 +1,192 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:formvalidation/src/models/producto_model.dart';
+import 'package:formvalidation/src/providers/productos_provider.dart';
+import 'package:formvalidation/src/utils/utils.dart' as utils;
+import 'package:image_picker/image_picker.dart';
+
+class ProductoPage extends StatefulWidget {
+  @override
+  _ProductoPageState createState() => _ProductoPageState();
+}
+
+class _ProductoPageState extends State<ProductoPage> {
+  final formKey = GlobalKey<FormState>();
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+  final productoProvider = new ProductosProvider();
+  ProductoModel producto = new ProductoModel();
+  bool _guardando = false;
+  File foto;
+
+  @override
+  Widget build(BuildContext context) {
+    final ProductoModel prodData = ModalRoute.of(context).settings.arguments;
+    if (prodData != null) {
+      producto = prodData;
+    }
+    return Scaffold(
+      key: scaffoldKey,
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.home),
+          onPressed: () => Navigator.pushReplacementNamed(context, 'home'),
+        ),
+        title: Text('Producto'),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.photo_size_select_actual),
+              onPressed: _seleccionarFoto),
+          IconButton(icon: Icon(Icons.camera_alt), onPressed: _tomarFoto),
+        ],
+      ),
+      //drawer: Icon(Icons.ac_unit),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(15.0),
+          child: Form(
+            key: formKey,
+            child: Column(
+              children: [
+                _mostrarFoto(),
+                _crearNombre(),
+                _crearPrecio(),
+                _crearDisponible(),
+                _crearBoton(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _crearNombre() {
+    return TextFormField(
+      initialValue: producto.titulo,
+      textCapitalization: TextCapitalization.sentences,
+      decoration: InputDecoration(
+        labelText: 'Producto',
+      ),
+      onSaved: (newValue) {
+        producto.titulo = newValue;
+      },
+      validator: (value) {
+        if (value.length < 3) {
+          return 'Ingrese un nombre válido';
+        } else {
+          return null;
+        }
+      },
+    );
+  }
+
+  Widget _crearPrecio() {
+    return TextFormField(
+      initialValue: producto.valor.toString(),
+      keyboardType: TextInputType.numberWithOptions(decimal: true),
+      decoration: InputDecoration(
+        labelText: 'Precio',
+      ),
+      onSaved: (newValue) {
+        producto.valor = double.parse(newValue);
+      },
+      validator: (value) {
+        if (utils.isNumeric(value)) {
+          return null;
+        } else {
+          return 'Sólo caracteres numéricos';
+        }
+      },
+    );
+  }
+
+  Widget _crearBoton() {
+    return RaisedButton.icon(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      color: Colors.deepPurple,
+      textColor: Colors.white,
+      label: Text('Guardar'),
+      icon: Icon(Icons.save),
+      onPressed: (_guardando) ? null : _submit,
+    );
+  }
+
+  Widget _crearDisponible() {
+    return SwitchListTile(
+      value: producto.disponible,
+      title: Text('Disponible'),
+      activeColor: Colors.deepPurple,
+      onChanged: (value) => setState(() {
+        producto.disponible = value;
+      }),
+    );
+  }
+
+  void _submit() async {
+    if (!formKey.currentState.validate()) return;
+
+    formKey.currentState.save();
+    setState(() {
+      _guardando = true;
+    });
+    if (foto != null) {
+      producto.fotoUrl = await productoProvider.subirImagen(foto);
+    }
+
+    if (producto.id == null) {
+      productoProvider.crearProducto(producto);
+    } else {
+      productoProvider.editarProducto(producto);
+    }
+    // setState(() {
+    //   _guardando = false;
+    // });
+    mostrarSnackbar('Registro guardado');
+    Navigator.pushReplacementNamed(context, 'home');
+  }
+
+  void mostrarSnackbar(String mensaje) {
+    final snackbar = SnackBar(
+      content: Text(mensaje),
+      duration: Duration(milliseconds: 1500),
+    );
+    scaffoldKey.currentState.showSnackBar(snackbar);
+  }
+
+  _mostrarFoto() {
+    if (producto.fotoUrl != null) {
+      return FadeInImage(
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          image: NetworkImage(producto.fotoUrl),
+          height: 300.0,
+          width: double.infinity,
+          fit: BoxFit.cover);
+    } else {
+      return Image(
+        image: AssetImage(foto?.path ?? 'assets/no-image.png'),
+        height: 300.0,
+        fit: BoxFit.cover,
+      );
+    }
+  }
+
+  _seleccionarFoto() async {
+    _procesarImage(ImageSource.gallery);
+  }
+
+  _tomarFoto() async {
+    _procesarImage(ImageSource.camera);
+  }
+
+  _procesarImage(ImageSource origen) async {
+    foto = await ImagePicker.pickImage(source: origen);
+    if (foto != null) {
+      producto.fotoUrl = null;
+    }
+
+    setState(() {});
+  }
+}
